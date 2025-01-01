630bbd023187e2e26c3a8c992ea7f31fac33755b
nadyacamila <nadyacamila47640@gmail.com>
2025-01-01 15:14:45 +0700

    nothing much

--- app/src/main/java/com/example/ecosynergy/DashboardActivity.java
+++ app/src/main/java/com/example/ecosynergy/DashboardActivity.java
@@ -1,4 +1,11 @@
 package com.example.ecosynergy;
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class DashboardActivity {
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+    }
 }
