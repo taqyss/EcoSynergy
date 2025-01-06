4146278f0adbe39232bdd98b347818668f8f8a22
Atiqah Maisarah <22003055@siswa.um.edu.my>
2025-01-06 17:26:50 +0800

    admin activity ui

--- app/src/main/java/com/example/ecosynergy/AdminActivity.java
+++ app/src/main/java/com/example/ecosynergy/AdminActivity.java
@@ -1,4 +1,20 @@
 package com.example.ecosynergy;
 
-public class AdminActivity {
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AdminActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Link the layout file to the activity
+        setContentView(R.layout.admin_page);  // Replace with the appropriate layout
+        setContentView(R.layout.content_management);
+        setContentView(R.layout.analytic_dashboard);
+
+        setupToolbar(true);
+        getSupportActionBar();
+        setupBottomNavigation();
+    }
 }
