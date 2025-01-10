8d28bb3bb3babfe87f777d198904b53d31ca7968
adhiml192 <112741074+adhiml192@users.noreply.github.com>
2025-01-11 02:03:30 +0800

    add drawables

--- app/src/main/java/com/example/ecosynergy/NavigationUtils.java
+++ app/src/main/java/com/example/ecosynergy/NavigationUtils.java
@@ -4,14 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 
 public class NavigationUtils {
-    public static Intent createSubCategoryModulesIntent(Context context, String selectedCategory, String hierarchy) {
-        Intent intent = new Intent(context, SubCategoryActivity.class);
-        intent.putExtra("CATEGORY_NAME", selectedCategory);
-        intent.putExtra("HIERARCHY", hierarchy);
-        return intent;
-    }
-
-    public static Intent createSubCategoryModulesIntent(Context context, String selectedCategory, String hierarchy) {
+    public static Intent createSubCategoryIntent(Context context, String selectedCategory, String hierarchy) {
         Intent intent = new Intent(context, SubCategoryActivity.class);
         intent.putExtra("CATEGORY_NAME", selectedCategory);
         intent.putExtra("HIERARCHY", hierarchy);
